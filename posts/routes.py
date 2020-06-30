c172649e1843393f8551404ff5758320e6d63e1f
Samantha-Kwok <szewingsamantha@gmail.com>
2020-06-30 14:36:16 +0100

    Update routes.py

--- posts/routes.py
+++ posts/routes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint,render_template, url_for, flash, redirect, request, abort
 from  todolist import  db
-from  todolist.posts.forms import PostForm
+from  todolist.posts.forms import PostForm, UpdatePostForm
 from  todolist.models import  Post
 from  flask_login import  current_user, login_required
+from  todolist.posts.utils import save_picture
 
 posts=Blueprint("posts", __name__)
 
@@ -23,7 +24,7 @@ def blog():
 def new_post():
     form=PostForm()   
     if form.validate_on_submit():
-        post=Post(title=form.title.data, content=form.content.data, author=current_user) 
+        post=Post(title=form.title.data, subtitle=form.subtitle.data, content=form.content.data, author=current_user) 
         db.session.add(post)
         db.session.commit()   
         flash("Your post has been created!", "success")
@@ -46,21 +47,29 @@ def your_post_content(post_id):
 @posts.route("/post/<int:post_id>/update", methods=[ "GET", "POST"])
 @login_required
 def update_post(post_id):
-    post=Post.query.get_or_404(post_id)
-    if post.author != current_user:
+    print ("step")
+    current_post=Post.query.get_or_404(post_id)
+    if current_post.author != current_user:
         abort(403)
-    form=PostForm()
+    form=UpdatePostForm()
+    print ("step1")
     if form.validate_on_submit():
-        post.title=form.title.data
-        post.content=form.content.data
+        print(form.picture.data)
+        if form.picture.data:
+            print("step3")
+            picture_file=save_picture(form.picture.data)
+            current_post.post_image=picture_file
+        current_post.title=form.title.data
+        current_post.subtitle=form.subtitle.data
+        current_post.content=form.content.data
         db.session.commit()
         flash("Your post has been updated!", "success")
-        return redirect(url_for("main.home"))
     elif request.method =="GET":
-        form.title.data=post.title
-        form.content.data=post.content
-    image_file =url_for("static", filename="profile_pics/" + current_user.image_file)
-    return render_template("update_post.html", title="Update Post", image_file=image_file, form=form)
+        form.title.data=current_post.title
+        form.subtitle.data=current_post.subtitle
+        form.content.data=current_post.content
+    post_image =url_for("static", filename="post_pics/" + current_post.post_image)
+    return render_template("update_post.html", title="Update Post", post_image=post_image, form=form)
     
 @posts.route("/post/<int:post_id>/delete", methods=["POST", "GET"])
 @login_required
