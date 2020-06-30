e58744864466cef3fa0ca6d22e845aa7165c71d1
Samantha-Kwok <szewingsamantha@gmail.com>
2020-06-30 14:35:17 +0100

    Update forms.py

--- posts/forms.py
+++ posts/forms.py
@@ -1,11 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import DataRequired, Length
-
+from flask_wtf.file import FileField, FileAllowed
 
 
 class PostForm(FlaskForm):
     title=StringField("Title", validators=[DataRequired(), Length(min=2, max=200)])
+    subtitle=StringField("Summary",validators=[DataRequired(),Length(min=2, max=100)])
     content=TextAreaField("Content", validators=[DataRequired()])
     submit=SubmitField("Submit")
 
+
+class UpdatePostForm(FlaskForm):
+    title=StringField("Title", validators=[DataRequired(), Length(min=2, max=100)])
+    subtitle=StringField("Summary",validators=[DataRequired(),Length(min=2, max=100)])
+    content=TextAreaField("Content", validators=[DataRequired()])
+    picture = FileField("Upload Your Post Picture", validators=[FileAllowed(["jpg", "png","jpeg"])])
+    submit=SubmitField("Update")
+
+ 
